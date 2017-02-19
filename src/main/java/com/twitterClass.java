7760b86ff206b584c2237a33e1f8a8419f8eef39
hadoop <gonzalobd>
2017-02-19 18:40:47 +0100

    second commit

--- src/main/java/com/twitterClass.java
+++ src/main/java/com/twitterClass.java
@@ -47,29 +47,35 @@ public class twitterClass {
         props_proc.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "serializers.JsonSerializer");
         props_proc.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, "serializers.SimplePartitioner");
         Producer<String, Map<String, Object>> producer = new KafkaProducer<>(props_proc);
-
+        Map<String, Object> tweetFilt = new HashMap<>();
         while (!closed.get()) {
             ConsumerRecords<String,Map<String,Object>> records = consumer.poll(100);
             for (ConsumerRecord<String, Map<String,Object>> record : records) {
-                System.out.println("Tweet readed and filtered in topic: "+record.topic()+record.value().get("created_at"));
-                System.out.println("readed"+record.value());
-                Map<String, Object> tweetFilt = new HashMap<>();
-                String created_at=record.value().get("created_at").toString();
-                tweetFilt.put("created_at",created_at);
-                if(record.value().get("coordinates")!=null){String coordinates=record.value().get("coordinates").toString();
-                tweetFilt.put("coordinates",coordinates);}
-                String lang=record.value().get("lang").toString();
-                tweetFilt.put("lang",lang);
-                if (record.value().get("place")!=null){String place=record.value().get("place").toString();
-                tweetFilt.put("place",place);}
-                Integer retweet_count=(Integer)record.value().get("retweet_count");
-                tweetFilt.put("retweet_count",retweet_count);
-                Integer favorite_count=(Integer)record.value().get("favorite_count");
-                tweetFilt.put("favorite_count",favorite_count);
-                String text=record.value().get("text").toString();
-                tweetFilt.put("text",text);
+                tweetFilt.clear();
+                System.out.println("Tweet readed from topic: "+record.topic());
+                if(record.value().get("created_at")!=null){
+                    String created_at=record.value().get("created_at").toString();
+                    tweetFilt.put("created_at",created_at);}
+                if(record.value().get("coordinates")!=null){
+                    String coordinates=record.value().get("coordinates").toString();
+                    tweetFilt.put("coordinates",coordinates);}
+                if(record.value().get("lang")!=null){
+                    String lang=record.value().get("lang").toString();
+                    tweetFilt.put("lang",lang);}
+                if (record.value().get("user.location")!=null){
+                    String place=record.value().get("user.location").toString();
+                    tweetFilt.put("user.location",place);}
+                if (record.value().get("retweet_count")!=null){
+                    Integer retweet_count=(Integer)record.value().get("retweet_count");
+                    tweetFilt.put("retweet_count",retweet_count);}
+                if (record.value().get("favorite_count")!=null){
+                    Integer favorite_count=(Integer)record.value().get("favorite_count");
+                    tweetFilt.put("favorite_count",favorite_count);}
+                if (record.value().get("text")!=null){
+                    String text=record.value().get("text").toString();
+                    tweetFilt.put("text",text);}
                 producer.send(new ProducerRecord<>(TOPIC_OUT, "Filtered_tweet", tweetFilt));
-
+                System.out.println("Tweet sent: "+tweetFilt.get("text"));
 
             }
         }
